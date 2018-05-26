a5a902f94aaf4259150d8ba120344365832421d8
XLILT <xiaolongma002@163.com>
2018-05-26 22:42:26 +0800

    record access log

---
+++ src/routes/access_recroder.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var router = express.Router();
+var db_cfg = require('../db/config');
+var mongodb = require('mongodb').MongoClient;
+
+var db = null,
+    dbDetails = new Object();
+
+var initDb = function() {
+  if (db_cfg.mongoURL == null) return;
+
+  if (mongodb == null) return;
+
+  mongodb.connect(db_cfg.mongoURL, function(err, conn) {
+    if (err) {
+      console.log(err)
+      return;
+    }
+
+    db = conn;
+    dbDetails.databaseName = db.databaseName;
+    dbDetails.url = mongoURLLabel;
+    dbDetails.type = 'MongoDB';
+
+    console.log('Connected to MongoDB at: %s', mongoURL);
+  });
+};
+
+router.use('/', function(req, res, next) {
+	if (!db) {
+		initDb();
+	}
+
+	if (db) {
+    	var col = db.collection('access_log');
+    	// Create a document with request IP and current time of request
+    	col.insert({ip: req.headers['x-forwarded-for'], url: req.originalUrl, refer: req.headers['referer'], date: Date.now()});
+    };
+
+	next();
+});
+
+module.exports = router;
